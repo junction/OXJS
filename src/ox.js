50e0a1074bdd53f8ffe21dee50677c0fd2470cd9
Brian Cully <bjc@kublai.com>
2009-02-24 16:23:04 -0500

    Add OX.Base.extend and OX.base.mixin.

--- src/ox.js
+++ src/ox.js
@@ -1,2 +1,40 @@
 /* Main application namespace. */
 var OX = {};
+
+/**
+ * Base object for OXJS. All other objects inherit from this one.
+ */
+OX.Base = {
+  /**
+   * Creates a new object which extends the current object.  Any
+   * arguments are mixed in to the new object as if +OX.Base.extend+ was
+   * called on the new object with remaining args.
+   *
+   * @return the new object
+   */
+  extend: function () {
+    var F = function () {};
+    F.prototype = this;
+
+    var rc = new F();
+    rc.mixin.apply(rc, arguments);
+
+    return rc;
+  },
+
+  /**
+   * Iterates over all arguments, adding their own properties to the
+   * receiver.
+   *
+   * @return the receiver
+   */
+  mixin: function () {
+    for (var i = 0, len = arguments.length; i < len; i++) {
+      for (var k in arguments[i]) if (arguments[i].hasOwnProperty(k)) {
+        this[k] = arguments[i][k];
+      }
+    }
+
+    return this;
+  }
+};
--- test/test.js
+++ test/test.js
@@ -11,6 +11,32 @@ OXTest.Namespace = new YAHOO.tool.TestCase({
   }
 });
 
+OXTest.OXBase = new YAHOO.tool.TestCase({
+  name: 'OX.Base Tests',
+
+  testExtend: function () {
+    var Assert = YAHOO.util.Assert;
+
+    var tmp = OX.Base.extend();
+    Assert.isObject(tmp);
+    Assert.isFunction(tmp.extend);
+    Assert.areNotSame(tmp, OX.Base);
+
+    var tmp2 = tmp.extend({foo: 'foo'});
+    Assert.areSame('foo', tmp2.foo);
+    Assert.isUndefined(tmp.foo);
+  },
+
+  testMixin: function () {
+    var Assert = YAHOO.util.Assert;
+
+    var tmp = OX.Base.extend();
+    tmp.mixin({foo: 'foo'});
+    Assert.areSame('foo', tmp.foo);
+  }
+});
+
 new YAHOO.tool.TestLogger();
 YAHOO.tool.TestRunner.add(OXTest.Namespace);
+YAHOO.tool.TestRunner.add(OXTest.OXBase);
 YAHOO.tool.TestRunner.run();
