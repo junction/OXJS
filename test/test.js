b2cd292770dc6626ea9336b2821d0d2ae2f5f2fd
Brian Cully <bjc@kublai.com>
2009-02-24 17:56:58 -0500

    Add OX URI tests

--- test/test.js
+++ test/test.js
@@ -47,6 +47,38 @@ OXTest.Error = new YAHOO.tool.TestCase({
   }
 });
 
+OXTest.URI = new YAHOO.tool.TestCase({
+  name: 'OX URI Tests',
+
+  testParse: function () {
+    var Assert = YAHOO.util.Assert;
+
+    Assert.isFunction(OX.URI.parse);
+  },
+
+  testToString: function () {
+    var Assert = YAHOO.util.Assert;
+
+    var uri = OX.URI.parse('xmpp://some@jid.com/test@foobar.com?message;subject=test;body=foobar');
+    Assert.isFunction(uri.toString);
+  },
+
+  testFromObject: function () {
+    var Assert = YAHOO.util.Assert;
+
+    var uri = OX.URI.extend({to:      'foo@bar.com',
+                             command: 'message'});
+    Assert.isObject(uri);
+    Assert.areSame('xmpp:foo@bar.com?message', uri.toString());
+  },
+
+  testParse: function () {
+    var Assert = YAHOO.util.Assert;
+
+    Assert.isFunction(OX.URI.parse);
+  }
+});
+
 OXTest.Services = new YAHOO.tool.TestCase({
   name: 'OX Service Tests',
 
