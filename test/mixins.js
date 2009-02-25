c5f4b5fbd5e1829bfa479b5136f938729ee5cbf5
Brian Cully <bjc@kublai.com>
2009-02-25 10:37:42 -0500

    Add nice result messages for mixins tests.

--- test/mixins.js
+++ test/mixins.js
@@ -4,31 +4,39 @@ OXTest.Mixins = new YAHOO.tool.TestCase({
   testInDialog: function () {
     var Assert = YAHOO.util.Assert;
 
-    Assert.isObject(OX.Mixins.InDialog);
-    Assert.isFunction(OX.Mixins.InDialog.transfer);
+    Assert.isObject(OX.Mixins.InDialog, 'InDialog mixin is not available.');
+    Assert.isFunction(OX.Mixins.InDialog.transfer,
+                      'InDialog.transfer is not a function.');
   },
 
   testPreDialog: function () {
     var Assert = YAHOO.util.Assert;
 
-    Assert.isObject(OX.Mixins.PreDialog);
-    Assert.isFunction(OX.Mixins.PreDialog.hangup);
+    Assert.isObject(OX.Mixins.PreDialog, 'PreDialog mixin is not available.');
+    Assert.isFunction(OX.Mixins.PreDialog.hangup,
+                      'PreDialog.hangup is not a function.');
   },
 
   testCallLabeler: function () {
     var Assert = YAHOO.util.Assert;
 
-    Assert.isObject(OX.Mixins.CallLabeler);
-    Assert.isFunction(OX.Mixins.CallLabeler.label);
+    Assert.isObject(OX.Mixins.CallLabeler,
+                    'CallLabeler mixin is not available.');
+    Assert.isFunction(OX.Mixins.CallLabeler.label,
+                      'CallLabeler.label is not a function.');
   },
 
   testSubscribable: function () {
     var Assert = YAHOO.util.Assert;
 
-    Assert.isObject(OX.Mixins.Subscribable);
-    Assert.isFunction(OX.Mixins.Subscribable.subscribe);
-    Assert.isFunction(OX.Mixins.Subscribable.unsubscribe);
-    Assert.isFunction(OX.Mixins.Subscribable.getItems);
+    Assert.isObject(OX.Mixins.Subscribable,
+                    'Subscribable mixin is not available.');
+    Assert.isFunction(OX.Mixins.Subscribable.subscribe,
+                      'Subscribable.subscribe is not a function.');
+    Assert.isFunction(OX.Mixins.Subscribable.unsubscribe,
+                      'Subscribable.unsubscribe is not a function.');
+    Assert.isFunction(OX.Mixins.Subscribable.getItems,
+                      'Subscribable.getItems is not a function.');
   }
 });
 
