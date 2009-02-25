59cf356224d2cb9f39b5c1c4a58eb975c17c7f95
Brian Cully <bjc@kublai.com>
2009-02-25 11:31:01 -0500

    Add tests for parse and unparse of URIs.
    Break URI parse tests out into simple/full.

--- test/uri.js
+++ test/uri.js
@@ -1,34 +1,51 @@
 OXTest.URI = new YAHOO.tool.TestCase({
   name: 'OX URI Tests',
 
-  testParse: function () {
+  testParseFull: function () {
     var Assert = YAHOO.util.Assert;
 
-    Assert.isFunction(OX.URI.parse, 'URI.parse is not a function.');
-  },
-
-  testToString: function () {
-    var Assert = YAHOO.util.Assert;
-
-    var uriString = 'xmpp://some@jid.com/test@foobar.com?message;subject=test;body=foobar';
+    var uriString = 'xmpp://some@jid.com/test@foobar.com?message;subject=test;body=foobar#frag';
     var uri = OX.URI.parse(uriString);
-    Assert.isFunction(uri.toString, 'uri.toString is not a function.');
+    Assert.areSame('xmpp', uri.scheme);
+    Assert.areSame('some@jid.com', uri.authority);
+    Assert.areSame('test@foobar.com', uri.path);
+    Assert.areSame('message;subject=test;body=foobar', uri.query);
+    Assert.areSame('frag', uri.fragment);
     Assert.areSame(uriString, uri.toString(),
                    'Unparse of parse did not produce the same string used to parse.');
   },
 
-  testFromObject: function () {
+  testParseSimple: function () {
     var Assert = YAHOO.util.Assert;
 
-    var uri = OX.URI.fromObject({to:      'foo@bar.com',
-                                 command: 'message'});
-    Assert.areSame('xmpp:foo@bar.com?message', uri.toString());
+    var uriString = 'xmpp:enoch@example.com';
+    var uri = OX.URI.parse(uriString);
+    Assert.areSame('xmpp', uri.scheme);
+    Assert.areSame('enoch@example.com', uri.path);
+    Assert.isUndefined(uri.authority);
+    Assert.isUndefined(uri.query);
+    Assert.isUndefined(uri.fragment);
+    Assert.areSame(uriString, uri.toString(),
+                   'Unparse of parse did not produce the same string used to parse.');
   },
 
-  testParse: function () {
+  testFromSimpleObject: function () {
     var Assert = YAHOO.util.Assert;
 
-    Assert.isFunction(OX.URI.parse, 'URI.parse is not a function.');
+    var uri = OX.URI.fromObject({path: 'foo@bar.com'});
+    Assert.areSame('xmpp:foo@bar.com', uri.toString());
+  },
+
+  testFromFullObject: function () {
+    var Assert = YAHOO.util.Assert;
+
+    var uri = OX.URI.fromObject({scheme:    'xmpp',
+                                 authority: 'enoch@example.com',
+                                 path:      'foo@bar.com',
+                                 query:     'message;subject=Hi',
+                                 fragment:  'baz'});
+    Assert.areSame('xmpp://enoch@example.com/foo@bar.com?message;subject=Hi#baz',
+                   uri.toString());
   }
 });
 
