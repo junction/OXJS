a3f92b18f0f0b1e380e32a67cd919f0a21509a25
Brian Cully <bjc@kublai.com>
2009-02-26 18:35:32 -0500

    Add OXTest.ConnectionMock.
    You can spy on sent XML via the _data property.

--- test/oxtest.js
+++ test/oxtest.js
@@ -1,2 +1,21 @@
 /* Namespace for OXJS tests. */
 var OXTest = {};
+
+OXTest.ConnectionMock = {
+  _handlers: {},
+
+  _data: null,
+
+  send: function (xml, callback, args) {
+    this._data = xml;
+    callback.apply(callback, args);
+  },
+
+  registerHandler: function (event, handler) {
+    this._handlers[event] = handler;
+  },
+
+  unregisterHandler: function (event, handler) {
+    delete this._handlers[event];
+  }
+};
