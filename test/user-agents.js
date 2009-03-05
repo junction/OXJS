94a2d6fba3fd183fb2b5bdf86a003ec5614aa69d
Brian Cully <bjc@kublai.com>
2009-03-05 12:34:06 -0500

    Remove user agent event slot from tests.

--- test/user-agents.js
+++ test/user-agents.js
@@ -37,7 +37,7 @@ OXTest.UserAgents = new YAHOO.tool.TestCase({
     Assert.isFunction(this.UserAgents.itemFromPacket,
                       'User agent service cannot turn packet into item.');
 
-    var packet = OXTest.Packet.extendWithXML('<message from="user-agents.xmpp.onsip.com" to="me@example.com"><event xmlns="http://jabber.org/protocol/pubsub#event"><items node="/example.com/me"><item id="sip:jill@example.com:5060"><user-agent xmlns="onsip:user-agents"><contact>sip:jill@example.com:5060</contact><received>sip:jack@example.com:5060</received><device>Test UA</device><expires>2009-03-02T15:58:23Z</expires><event>insert</event></user-agent></item></items></event></message>');
+    var packet = OXTest.Packet.extendWithXML('<message from="user-agents.xmpp.onsip.com" to="me@example.com"><event xmlns="http://jabber.org/protocol/pubsub#event"><items node="/example.com/me"><item id="sip:jill@example.com:5060"><user-agent xmlns="onsip:user-agents"><contact>sip:jill@example.com:5060</contact><received>sip:jack@example.com:5060</received><device>Test UA</device><expires>2009-03-02T15:58:23Z</expires></user-agent></item></items></event></message>');
     var item = this.UserAgents.itemFromPacket(packet);
     Assert.isObject(item, 'UserAgents.itemFromPacket did not return an object.');
     Assert.areSame(this.ox, item.connection,
@@ -48,7 +48,6 @@ OXTest.UserAgents = new YAHOO.tool.TestCase({
                    'Received is wrong.');
     Assert.areSame('Test UA', item.device, 'Device is wrong.');
     Assert.aresame('2009-03-02T15:58:23Z', item.expires, 'Expiry is wrong.');
-    Assert.areSame('insert', item.event, 'Event type is wrong.');
   },
 
   testContact: function () {
@@ -77,13 +76,6 @@ OXTest.UserAgents = new YAHOO.tool.TestCase({
 
     Assert.isNotUndefined(this.UserAgents.Item.expires,
                           'UserAgents.Item.expires is undefined');
-  },
-
-  testEvent: function () {
-    var Assert = YAHOO.util.Assert;
-
-    Assert.isNotUndefined(this.UserAgents.Item.event,
-                          'UserAgents.Item.event is undefined');
   }
 });
 
