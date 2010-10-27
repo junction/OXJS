491a7bdaf8dc736224d2dcaa37d5920e890f428d
Tim Evans <tim.evans@junctionnetworks.com>
2010-10-27 14:46:49 -0400

    Added a Strophe Adapter for OX.

---
+++ src/strophe_adapter.js
@@ -0,0 +1,206 @@
+/**
+ * Strophe Connection Adapter
+ * @class
+ * @extends OX.ConnectionAdapter
+ */
+OX.StropheAdapter = OX.ConnectionAdapter.extend(
+  /** @lends OX.StropheAdapter# */{
+
+  /** @private */
+  _callbacks: {},
+  /** @private */
+  _handlers: {},
+
+  _callbackQueue: [],
+  MAX_QUEUE_SIZE: 100,
+
+  /** @private */
+  init: function () {
+    this._callbacks = {};
+    this._handlers = {};
+    this._callbackQueue = [];
+  },
+
+  /**
+   * @returns {String} The JID associated with the connection.
+   */
+  jid: function () {
+    return this.strophe.jid;
+  },
+
+  /**
+   * Subscribe to stanza via top-level XMPP tag name.
+   *
+   * @param {String} event The top level XMPP tag name to register for.
+   * @param {Function} handler The function handler for the event.
+   */
+  registerHandler: function (event, handler) {
+    var that = this;
+
+    var wrapper = function (stanza) {
+      var packetAdapter = that.toPacket(stanza),
+          newArgs = [packetAdapter];
+
+      for (var i = 1, len = arguments.length; i < len; i++) {
+        newArgs.push(arguments[i]);
+      }
+
+      try {
+        handler.apply(this, newArgs);
+      } catch (e) {
+        OX.error('Error in OX handler: ' + handler +
+                 '; Error: ' + e + '; response stanza: ' + stanza);
+      }
+      return true;
+    };
+
+    this.unregisterHandler(event);
+    this._handlers[event] = this.strophe.addHandler(wrapper, null, event,
+                                                    null, null, null);
+  },
+
+  /**
+   * Unsubscribe from corresponding event.
+   *
+   * @param {String} event The event to unsubscribe from.
+   */
+  unregisterHandler: function (event) {
+    var queue = this._callbackQueue, i, len = queue.length, rest;
+
+    if (this._handlers[event]) {
+      this.strophe.deleteHandler(this._handlers[event]);
+      delete this._handlers[event];
+
+      // Remove from the callback queue
+      for (i = 0; i < len; i++) {
+        if (queue[i] === event) {
+          rest = queue.slice(i + 1, queue.length);
+          queue.length = i;
+          queue.push(rest);
+          break;
+        }
+      }
+    }
+  },
+
+  /**
+   * Create a DOM node via XML.
+   *
+   * @private
+   * @param {String} xml The xml string to convert into an object.
+   * @returns {Element} The document fragment that represents the xml string.
+   */
+  createNode: function (xml) {
+    var node = null, parser = null;
+    if (window.ActiveXObject) {
+      parser = new ActiveXObject("Microsoft.XMLDOM");
+      parser.async = "false";
+      parser.setProperty("SelectionLanguage", "XPath");
+      parser.loadXML(xml);
+      node = parser.firstChild;
+    } else {
+      parser = new DOMParser();
+      node = parser.parseFromString(xml, 'text/xml');
+      node = node.firstChild;
+      document.adoptNode(node);
+    }
+    return node;
+  },
+
+  /**
+   * Send the xml fragment over the connection.
+   *
+   * @param {String} xml The xml to send.
+   * @param {Function} callback The function to call when done.
+   * @param {Array} args A list of arguments to provide to the callback.
+   */
+  send: function (xml, callback, args) {
+    var node = this.createNode(xml),
+        that = this;
+
+    if (!this.strophe.connected || this.strophe.disconnecting) {
+      OX.log('Prevented "' + xml + '" from being sent because ' +
+             'the BOSH connection is being disposed / is disposed.');
+      return false;
+    }
+
+    if (callback) {
+      var wrapper = function (stanza) {
+        var packetAdapter = that.toPacket(stanza),
+            newArgs = [packetAdapter],
+            queue = that._callbackQueue, rest,
+            event = node.getAttribute('id'), i, len;
+        args = args || [];
+        for (i = 0, len = args.length; i < len; i++) {
+          newArgs.push(args[i]);
+        }
+
+        try {
+          callback.apply(this, newArgs);
+        } catch (e) {
+          OX.error('Error in OX handler: ' + callback +
+                   '; Error: ' + e + '; response stanza: ' + stanza);
+        }
+
+        // Remove from the callback queue
+        for (i = 0, len = queue.length; i < len; i++) {
+          if (queue[i].toString() === event) {
+            rest = queue.slice(i + 1, queue.length);
+            queue.length = i;
+            queue.push.apply(queue, rest);
+            break;
+          }
+        }
+
+        delete that._callbacks[event];
+
+        return false;
+      };
+
+      var id = node.getAttribute('id');
+      if (!id) {
+        id = this.strophe.getUniqueId();
+        node.setAttribute('id', id);
+      }
+
+      this._callbacks[id] = this.strophe.addHandler(wrapper, null, null,
+                                                       null, id, null);
+      this._callbackQueue.unshift(id);
+      if (this._callbackQueue.length > this.MAX_QUEUE_SIZE) {
+        OX.warn("You have too many callbacks waiting for a response, so I'm getting rid of the oldest one.\n" +
+                "If this isn't desired, override the MAX_QUEUE_SIZE of OX.StropheAdapter.");
+        delete this._callbacks[this._callbackQueue.pop()];
+      }
+    }
+    node.setAttribute('xmlns', 'jabber:client');
+    return this.strophe.send(node);
+  },
+
+  /**
+   * Convert a stanza into an object that implements {@link OX.PacketAdapter}.
+   *
+   * @private
+   * @param {Element} stanza The XMPP stanza to pack.
+   *
+   * @returns {OX.PacketAdapter} The stanza wrapped as a packet.
+   */
+  toPacket: function (stanza) {
+    var to = stanza.getAttribute('to'),
+        from = stanza.getAttribute('from'),
+        type = stanza.getAttribute('type');
+    return {
+      getFrom: function () {
+        return from;
+      },
+      getType: function () {
+        return type;
+      },
+      getTo: function () {
+        return to;
+      },
+      getNode: function () {
+        return stanza;
+      }
+    };
+  }
+});
---
+++ test/strophe_adapter.js
@@ -0,0 +1,72 @@
+/*global YAHOO*/
+OXTest.StropheAdapter = new YAHOO.tool.TestCase({
+  name: 'OX Strophe Adapter Tests',
+  setUp: function () {
+    this.conn = OX.MockStropheConnection.extend();
+    this.adapter = OX.StropheAdapter.extend({
+      connection: this.conn
+    });
+  },
+
+  tearDown: function () {
+    this.conn = null;
+    this.adapter = null;
+  },
+
+  testQueueSize: function () {
+    var Assert = YAHOO.util.Assert,
+        empty = function () {}, id;
+    this.adapter.send("<test>", empty);
+    Assert.areSame(1, this.adapter._callbackQueue.length);
+    id = this.conn.recentlyRegistered;
+    this.conn.triggerEvent(id, OX.Test.Packet.extendWithXML("<iq id=\"" + id + "\"/>").getNode());
+    Assert.areSame(0, this.adapter._callbackQueue.length);
+
+    this.adapter.send("<test>", empty);
+    this.adapter.send("<test>", empty);
+    id = this.conn.recentlyRegistered;
+    this.conn.triggerEvent(id, OX.Test.Packet.extendWithXML("<iq id=\"" + id + "\"/>").getNode());
+    Assert.areSame(1, this.adapter._callbackQueue.length);
+  },
+
+  testQueueLimit: function () {
+    var Assert = YAHOO.util.Assert,
+        empty = function () {};
+    this.adapter.send("<test>", empty);
+
+    for (var i = 1; i < 125; i++) {
+      Assert.areSame(i > 100 ? 100 : i, this.adapter._callbackQueue.length);
+      this.adapter.send("<test>", empty);
+    }
+    Assert.areSame(100, this.adapter._callbackQueue.length);
+  }
+});
+
+OX.MockStropheConnection = OX.Base.extend({
+  jid: 'mock@example.com',
+
+  _handlers: {},
+  recentlyRegistered: null,
+
+  addHandler: function (callback, foo, bar, baz, event) {
+    this.recentlyRegistered = event;
+    this._handlers[event] = callback;
+  },
+  deleteHandler: function () {},
+
+  connected: true,
+  disconnecting: false,
+
+  triggerEvent: function (event, response) {
+    this._handlers[event](response);
+  },
+
+  _id: 0,
+  getUniqueId: function () {
+    return this._id++;
+  },
+
+  send: function () {}
+});
+
+YAHOO.tool.TestRunner.add(OX.Test.StropheAdapter);
